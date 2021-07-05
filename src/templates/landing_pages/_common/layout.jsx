f6a683892f1c18c1ebb0bd2301fc0def29e92d31
Khalid Ibrahim <FXTRyio@gmail.com>
2021-07-05 18:15:54 +0800

    add survey popup (#6651)

--- src/templates/landing_pages/_common/layout.jsx
+++ src/templates/landing_pages/_common/layout.jsx
@@ -2,6 +2,7 @@ import React                  from 'react';
 import OutdatedBrowserMessage from './outdated_browser_message.jsx';
 import Title                  from '../../_common/components/title.jsx';
 import AntiClickjack          from '../../_common/includes/anti_clickjack.jsx';
+import SurveyPopUp            from '../../_common/includes/survey_pop_up.jsx';
 import Favicons               from '../../_common/includes/favicons.jsx';
 
 const Layout = ({
@@ -13,6 +14,7 @@ const Layout = ({
     <html>
         <head>
             <AntiClickjack />
+            <SurveyPopUp />
 
             <meta httpEquiv='Content-Type' content='text/html;charset=UTF-8' />
             <meta httpEquiv='Content-Language' content={it.language} />
