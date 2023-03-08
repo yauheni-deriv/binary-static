9d317339dc5099a79d0be8e53060cddd5c040135
Matin shafiei <matin@binary.com>
2023-03-08 13:54:34 +0800

    Revert "NikitK/ binary.com - Options for Branding Removal  (#7274)" (#7349)
    This reverts commit e024755c4c4c3854262a8c2de41a3f107058ec3b.

--- src/javascript/app/base/page.js
+++ src/javascript/app/base/page.js
@@ -84,12 +84,6 @@ const Page = (() => {
     };
 
     const onLoad = () => {
-        const host_name = $(location).attr('hostname');
-        const removeBranding = () => {
-            if (host_name !== 'localhost' && host_name !== 'binary.com' && host_name !== 'staging.binary.com' && !host_name.includes('binary-static-git-fork')) {
-                $('.remove-branding').remove();
-            }
-        };
         if (State.get('is_loaded_by_pjax')) {
             Url.reset();
             updateLinksURL('#content');
@@ -102,7 +96,6 @@ const Page = (() => {
                     // InterviewPopup.onLoad();
                 }
             }
-            removeBranding();
             Header.onLoad();
             Footer.onLoad();
             Language.setCookie();
